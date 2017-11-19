cc96e10acd398835435486e3846795860cbb0a4c
Markas Korotkovas <mkorotkovas@yahoo.com>
2017-11-18 17:06:29 -0700

    tweaked the PI detection a bit more

--- src/main/java/com/marasm/lcd4pi/LCD.java
+++ src/main/java/com/marasm/lcd4pi/LCD.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import com.marasm.lcd4pi.RealLCD.Direction;
 import com.marasm.logger.AppLogger;
+import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 import com.pi4j.system.SystemInfo;
 
 public interface LCD {
@@ -47,23 +48,37 @@ public interface LCD {
 	int buttonsPressedBitmask() throws IOException;
 	
 	
-	
-	public static LCD getInstance()
+	public static boolean isRunningOnPi()
 	{
-	  //if running on the PI return real impl
-	  String hardware;
-	  try
-	  {
-	    hardware = SystemInfo.getHardware();
-	    AppLogger.debug("Hardware: " + hardware);
-	  }
-	  catch(Exception e)
-	  {
-	    AppLogger.warn("Error getting hardware! Assuming that this is not a PI");
-	  }
-	  
-	  //otherwise
-	  return new MockupLCD();
+	  String hardware = null;
+    try
+    {
+      hardware = SystemInfo.getHardware();
+      AppLogger.debug("Hardware: " + hardware);
+    }
+    catch(Exception e)
+    {
+      AppLogger.warn("Error getting hardware! Assuming that this is not a PI");
+    }
+    return hardware != null && hardware.contains("BCM"); //BCM2708...
+	}
+	
+	
+	public static LCD getInstance() throws IOException, UnsupportedBusNumberException
+	{
+	  if (isRunningOnPi())
+	    return new RealLCD();
+	  else
+	    return new MockupLCD();
+	}
+	
+	public static LCD getInstance(int inBus, int inAddr) 
+	  throws IOException, UnsupportedBusNumberException
+	{
+	  if (isRunningOnPi())
+	    return new RealLCD(inBus, inAddr);
+	  else
+	    return new MockupLCD(inBus, inAddr);
 	}
 
 }
